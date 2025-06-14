83bd8ff3e5f7f2bc63ed81308ab2146b7df6bd4f
babanini95 <arobbanii5@gmail.com>
2025-06-15 00:12:32 +0800

    add readiness endpoint

--- main.go
+++ main.go
@@ -9,8 +9,15 @@ func main() {
 		Addr:    ":8080",
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	srv.ListenAndServe()
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
