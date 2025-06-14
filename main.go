814f07be62f5cf8f2d9a7f9cc1491b8573f549a5
babanini95 <arobbanii5@gmail.com>
2025-06-14 21:47:33 +0800

    use FileServer to serve an HTML page

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	srv.ListenAndServe()
 }
