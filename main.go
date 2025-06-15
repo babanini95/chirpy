faa3dbe42dac6fe43a789619f7487f00b9a01888
babanini95 <arobbanii5@gmail.com>
2025-06-15 09:28:47 +0800

    keep track how many /app/ get hitted by add a middleware

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -8,10 +16,14 @@ func main() {
 		Handler: mux,
 		Addr:    ":8080",
 	}
+	apiCfg := &apiConfig{}
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
 	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.writeNumberOfRequestHandler)
+	mux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
 
 	srv.ListenAndServe()
 }
@@ -20,4 +32,23 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) writeNumberOfRequestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("handler hit: %v\n", cfg.fileserverHits.Load())
+	w.Write(fmt.Appendf(nil, "Hits: %v", cfg.fileserverHits.Load()))
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		cfg.fileserverHits.Add(1)
+	})
 }
